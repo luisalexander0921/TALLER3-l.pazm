d5379ff800c759377d39d783b1e6b7d959c76eeb
Luis Alexander Paz Molina <pazla@globalhitss.com>
2024-11-19 21:38:24 -0500

    Un pequeño cambio
    Un pequeño cambio

---
+++ boa_constrictor.py
@@ -0,0 +1,18 @@
+from animal_exotico import AnimalExotico
+
+class BoaConstrictor(AnimalExotico):
+    def __init__(self, nombre, peso, edad, pais_origen:str, impuestos:float):
+        super().__init__(nombre, peso, edad, pais_origen, impuestos)
+        self._ratones_comidos = 0
+        
+    def hacer_sonido(self):
+        return "¡Tsss!"
+
+    def comer_raton(self, raton:int):
+        self._ratones_comidos += raton
+        
+    def ratones_comidos(self):
+        return self._ratones_comidos
+    
+
+    
